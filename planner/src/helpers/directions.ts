932d9c9456623f4f21228b37757bc758822903c7
anzo-p <anssi.pitkan@gmail.com>
2024-04-29 22:28:37 +0300

    planner - unexpected detour detection

--- planner/src/helpers/directions.ts
+++ planner/src/helpers/directions.ts
@@ -1,4 +1,10 @@
-import { RouteWaypoint } from '@/src/types/journey';
+import { Coordinate, RouteWaypoint } from '@/src/types/journey';
+
+import { bearingDeviation, calculateBearing } from './location';
+
+const angleThreshold = Number(process.env.NEXT_PUBLIC_ROUTE_DEVIATION_ANGULAR_LIMIT);
+const shortLegLimit = Number(process.env.NEXT_PUBLIC_ROUTE_DEVIATION_IGNORE_LEGS_SHORTER_THAN);
+const shortStepLimit = Number(process.env.NEXT_PUBLIC_ROUTE_DEVIATION_IGNORE_STEPS_SHORTER_THAN);
 
 export const getMapBounds = (waypoints: RouteWaypoint[]) => {
   const bounds = new window.google.maps.LatLngBounds();
@@ -13,3 +19,44 @@ export const calculateTotalDistance = (directions: google.maps.DirectionsResult)
     const distance = route.legs[0].distance ? route.legs[0].distance.value : Infinity;
     return agg + distance;
   }, 0);
+
+export const detectDetour = (directions: google.maps.DirectionsResult): boolean => {
+  const lastLeg: google.maps.DirectionsLeg = directions.routes[0].legs[directions.routes[0].legs.length - 1];
+
+  if (lastLeg.steps.length < 2) {
+    return false;
+  }
+  if ((lastLeg.distance?.value ?? 0) < shortLegLimit) {
+    return false;
+  }
+  if (lastLeg.start_address.split(' ')[0] !== lastLeg.end_address.split(' ')[0]) {
+    return false;
+  }
+
+  const startCoordinate: Coordinate = {
+    latitude: lastLeg.start_location.lat(),
+    longitude: lastLeg.start_location.lng()
+  };
+  const legBearing = calculateBearing(startCoordinate, {
+    latitude: lastLeg.end_location.lat(),
+    longitude: lastLeg.end_location.lng()
+  });
+  const invertedBearing = (legBearing + 180) % 360;
+  const excludeShortSteps = lastLeg.steps.filter(
+    (step: google.maps.DirectionsStep) => step?.distance?.value ?? 0 > shortStepLimit
+  );
+
+  return excludeShortSteps.some((step: google.maps.DirectionsStep) => {
+    const stepBearing = calculateBearing(startCoordinate, {
+      latitude: step.start_location.lat(),
+      longitude: step.start_location.lng()
+    });
+
+    const deviationFromStart = bearingDeviation(stepBearing, legBearing);
+    const deviationFromEnd = bearingDeviation(stepBearing, invertedBearing);
+
+    if (deviationFromStart > angleThreshold && deviationFromEnd > angleThreshold) {
+      return true;
+    }
+  });
+};
--- planner/src/helpers/location.ts
+++ planner/src/helpers/location.ts
@@ -3,3 +3,21 @@ import { Coordinate } from '@/src/types/journey';
 export const euclidianDistance = (a: Coordinate, b: Coordinate) => {
   return Math.sqrt(Math.pow(a.latitude - b.latitude, 2) + Math.pow(a.longitude - b.longitude, 2));
 };
+
+export const calculateBearing = (a: Coordinate, b: Coordinate): number => {
+  const rad = Math.PI / 180;
+  const phi1 = a.latitude * rad;
+  const phi2 = b.latitude * rad;
+  const deltaLambda = (b.longitude - a.longitude) * rad;
+
+  const y = Math.sin(deltaLambda) * Math.cos(phi2);
+  const x = Math.cos(phi1) * Math.sin(phi2) - Math.sin(phi1) * Math.cos(phi2) * Math.cos(deltaLambda);
+  const theta = Math.atan2(y, x);
+
+  return (theta * (180 / Math.PI) + 360) % 360;
+};
+
+export const bearingDeviation = (bearing: number, reference: number): number => {
+  const bearingDifference = Math.abs(bearing - reference);
+  return bearingDifference > 180 ? 360 - bearingDifference : bearingDifference;
+};
